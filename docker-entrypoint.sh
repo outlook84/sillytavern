85f3cb374569a28518b49c56a1179ee2c6f6d35e
outlook84 <96007761+outlook84@users.noreply.github.com>
2025-07-16 07:47:27 +0000

    password generation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,10 @@ fi
 
 sed -i 's/listen: false/listen: true/' "./config/config.yaml" || true
 sed -i 's/whitelistMode: true/whitelistMode: false/' "./config/config.yaml" || true
+sed -i 's/basicAuthMode: false/basicAuthMode: true/' "./config/config.yaml" || true
+
+PASSWORD=$(head -c 128 /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 16)
+sed -i 's/password: "password"/password: "'"$PASSWORD"'"/' "./config/config.yaml" || true
 
 # Execute postinstall to auto-populate config.yaml with missing values
 npm run postinstall
